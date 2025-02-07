af2ba03a99604197b7c1b38374aaa15e9391d20f
HeeJava <heejaeq@gmail.com>
2025-02-07 17:44:32 +0900

    ver1.0

--- src/main/java/com/example/ssauc/controller/MainController.java
+++ src/main/java/com/example/ssauc/controller/MainController.java
@@ -10,4 +10,22 @@ public class MainController {
 
     @GetMapping("signIn")
     public String login() {return "user/signIn"; }
+
+    @GetMapping("signUp")
+    public String signUp() {return "user/signUp"; }
+
+    @GetMapping("mypage")
+    public String mypage() {return "user/mypage"; }
+
+    @GetMapping("product")
+    public String product() {return "product/productList"; }
+
+    @GetMapping("community")
+    public String community() {return "community/community"; }
+
+    @GetMapping("cart")
+    public String basket() {return "product/cart"; }
+
+    @GetMapping("like")
+    public String like() {return "product/like"; }
 }
